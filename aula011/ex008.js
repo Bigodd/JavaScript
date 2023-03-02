a5eafe7ec299706d288bd40f45f274666fd6dae6
Gabriel Santos <gabrielssantos_97@outlook.com>
2023-03-02 12:53:03 -0300

    aprendendo condições

---
+++ aula011/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60.5
+console.log (`A velocidade do seu carro é ${vel} KM/h`)
+
+if (vel > 60) {
+    console.log ('Você ultrapassou o limite de velocidade. MULTADO')
+} 
+
+console.log ('dirija sempre usando cinto de segurança')
+
